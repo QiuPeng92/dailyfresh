c3716d2a54c93f640417acb4d6aad275b280d3a6
alex <976384846@qq.com>
2020-09-20 22:25:29 +0800

    添加models，并生成迁移文件

--- dailyfresh/urls.py
+++ dailyfresh/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tinymce/', include('tinymce.urls')),  # 富文本编辑器
+    path('user/', include(('user.urls', 'user'), namespace='user')),  # 用户模块
+    path('order/', include(('order.urls', 'order'), namespace='order')),  # 订单模块
+    path('cart/', include(('cart.urls', 'cart'), namespace='cart')),  # 购物车模块
+    path('', include(('goods.urls', 'goods'), namespace='goods')),  # 商品模块
 ]
