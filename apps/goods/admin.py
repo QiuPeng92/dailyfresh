b858598cd4c9613efda1578fc5c5f17b68957e5e
alex <976384846@qq.com>
2020-10-11 14:44:36 +0800

    添加首页静态和首页内容缓存

--- apps/goods/admin.py
+++ apps/goods/admin.py
@@ -1,6 +1,48 @@
 from django.contrib import admin
-
+from django.core.cache import cache
 # Register your models here.
-from goods.models import GoodsType
+from goods.models import GoodsType, IndexPromotionBanner, IndexTypeGoodsBanner, IndexGoodsBanner
 
-admin.site.register(GoodsType)
+
+class BaseModelAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        '''新增或者更新表中的数据时调用'''
+        super().save_model(request, obj, form, change)
+        # 发出任务，让celery worker重新生成首页静态页面
+        from celery_tasks.tasks import generate_static_index_html
+        generate_static_index_html.delay()
+
+        # 清除首页的缓存数据
+        cache.delete('index_page_data')
+
+    def delete_model(self, request, obj):
+        '''删除表中的数据时调用'''
+        super().delete_model(request, obj)
+        # 发出任务，让celery worker重新生成首页静态页面
+        from celery_tasks.tasks import generate_static_index_html
+        generate_static_index_html.delay()
+
+        # 清除首页的缓存数据
+        cache.delete('index_page_data')
+
+
+class GoodsTypeAdmin(BaseModelAdmin):
+    pass
+
+
+class IndexPromotionBannerAdmin(BaseModelAdmin):
+    pass
+
+
+class IndexGoodsBannerAdmin(BaseModelAdmin):
+    pass
+
+
+class IndexTypeGoodsBannerAdmin(BaseModelAdmin):
+    pass
+
+
+admin.site.register(GoodsType, GoodsTypeAdmin)
+admin.site.register(IndexPromotionBanner, IndexPromotionBannerAdmin)
+admin.site.register(IndexGoodsBanner, IndexTypeGoodsBannerAdmin)
+admin.site.register(IndexTypeGoodsBanner, IndexTypeGoodsBannerAdmin)
--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
-from goods import views
+from goods.views import IndexView, DetailView, ListView
 
 urlpatterns = [
-    re_path('^$', views.index, name= "index"),
+    re_path('^index$', IndexView.as_view(), name="index"),  # 首页
+    re_path('^goods/(?P<goods_id>\d+)$', DetailView.as_view(), name="detail"),  # 详情页
 ]
